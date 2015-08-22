db4ddc6b813df46a8fe96904737871a9c32c9745
Jikku Jose <jikkujose@gmail.com>
2015-08-22 19:45:51 +0530

    Add style for dashboard

--- app/main/controllers/dashboard_controller.rb
+++ app/main/controllers/dashboard_controller.rb
@@ -4,7 +4,30 @@ module Main
 
       page._address = 'Kowdiar, Trivandrum'
       page._markers << "Medical College, Trivandrum"
+      # page._events = []
+      populate_table
+    end
 
+    def populate_table
+      %w[
+
+      ].each do |name|
+        page._events << {name: name, glyph: 'phone', location: random_location}
+      end
+      p page._events
+    end
+
+    def random_location
+      %w[
+      kowdiar
+      kudappanakunnu
+      thambanoor
+      thirumala
+      ].sample
+    end
+
+    def fire
+      puts 'thats cool'
     end
   end
 end
